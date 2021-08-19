6f8b6bcb66bcae87657aa5b350a835d09a346abc
Furuya Ko <k.furuya.445@gmail.com>
2021-08-20 02:47:16 +0900

    SDL2 Tutorial-09

--- src/main.cpp
+++ src/main.cpp
@@ -120,14 +120,14 @@ bool loadMedia(){
         printf( "Failed to load right image!\n" );
         success = false;
     }
-
+    */
     // PNGテキスチャを読み込む
     gTexture = loadTexture( "./media/6star.png" );
     if( gTexture == NULL ){
         printf( "Failed to load texture image!\n" );
         success = false;
     }
-    */
+    
     return success;
 }
 
@@ -140,10 +140,10 @@ void close(){
         gKeyPressSurfaces[i] = NULL;
     }
     gCurrentSurface = NULL;
-
+    */
     SDL_DestroyTexture( gTexture );
     gTexture = NULL;
-    */
+    
 
     // ウインドウの破棄
     SDL_DestroyRenderer( gRenderer );
@@ -263,47 +263,41 @@ int main(int argc, char ** const args){
                 SDL_SetRenderDrawColor( gRenderer, 0xFF, 0xFF, 0xFF, 0xFF );
                 SDL_RenderClear( gRenderer );
 
-                // 赤く塗りつぶされた四角
-                SDL_Rect fillRect = { SCREEN_WIDTH/4, SCREEN_HEIGHT/4, SCREEN_WIDTH/2, SCREEN_HEIGHT/2 };
-                SDL_SetRenderDrawColor( gRenderer, 0xFF, 0x00, 0x00, 0xFF);
-                SDL_RenderFillRect( gRenderer, &fillRect );
+                // 左上コーナーのビューポート
+                SDL_Rect topLeftViewport;
+                topLeftViewport.x = 0;
+                topLeftViewport.y = 0;
+                topLeftViewport.w = SCREEN_WIDTH/2;
+                topLeftViewport.h = SCREEN_HEIGHT/2;
+                SDL_RenderSetViewport( gRenderer, &topLeftViewport );
 
-                // 緑色の縁の四角
-                SDL_Rect outlineRect = { SCREEN_WIDTH/6, SCREEN_HEIGHT/6, SCREEN_WIDTH*2/3, SCREEN_HEIGHT*2/3 };
-                SDL_SetRenderDrawColor( gRenderer, 0x00, 0xFF, 0x00, 0xFF);
-                SDL_RenderDrawRect( gRenderer, &outlineRect );
+                // スクリーンにテキスチャーをレンダーする
+                SDL_RenderCopy( gRenderer, gTexture, NULL, NULL);
 
-                // 青い水平線を描画
-                SDL_SetRenderDrawColor( gRenderer, 0x00, 0x00, 0xFF, 0xFF );
-                SDL_RenderDrawLine( gRenderer, 0, SCREEN_HEIGHT/2, SCREEN_WIDTH, SCREEN_HEIGHT/2);
+                // 右上コーナーのビューポート
+                SDL_Rect topRightViewport;
+                topRightViewport.x = SCREEN_WIDTH/2;
+                topRightViewport.y = 0;
+                topRightViewport.w = SCREEN_WIDTH/2;
+                topRightViewport.h = SCREEN_HEIGHT/2;
+                SDL_RenderSetViewport( gRenderer, &topRightViewport );
 
-                // 黄色いドットを縦線として描画
-                SDL_SetRenderDrawColor( gRenderer, 0xFF, 0xFF, 0x00, 0xFF);
-                for( int i = 0; i < SCREEN_HEIGHT; i += 4){
-                    SDL_RenderDrawPoint( gRenderer, SCREEN_WIDTH/2, i);
-                }
+                // スクリーンにテキスチャーをレンダーする
+                SDL_RenderCopy( gRenderer, gTexture, NULL, NULL);
 
-                // テキスチャーをスクリーンにレンダー
-                // SDL_RenderCopy( gRenderer, gTexture, NULL, NULL );
+                // 下コーナーのビューポート
+                SDL_Rect bottomViewport;
+                bottomViewport.x = 0;
+                bottomViewport.y = SCREEN_HEIGHT/2;
+                bottomViewport.w = SCREEN_WIDTH;
+                bottomViewport.h = SCREEN_HEIGHT/2;
+                SDL_RenderSetViewport( gRenderer, &bottomViewport );
+
+                // スクリーンにテキスチャーをレンダーする
+                SDL_RenderCopy( gRenderer, gTexture, NULL, NULL);
 
                 // スクリーンの更新
                 SDL_RenderPresent( gRenderer );
-                /*
-                // 画像のサイズ変更を適用する
-                SDL_Rect stretchRect;
-                stretchRect.x = 0;
-                stretchRect.y = 0;
-                stretchRect.w = SCREEN_WIDTH;
-                stretchRect.h = SCREEN_HEIGHT;
-                SDL_BlitScaled( gCurrentSurface, NULL, gScreenSurface, &stretchRect );
-
-
-                // 画像を表示する
-                // SDL_BlitSurface( gCurrentSurface, NULL, gScreenSurface, NULL);
-
-                // サーフェイスの更新
-                SDL_UpdateWindowSurface( gWindow );
-                */
             }
             
         }
