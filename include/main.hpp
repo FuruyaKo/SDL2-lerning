10d0d010244310897d1622c6ef57b9eb7dd0c636
Furuya Ko <k.furuya.445@gmail.com>
2021-08-20 03:25:28 +0900

    SDL2 Tutorial-10

--- include/main.hpp
+++ include/main.hpp
@@ -14,3 +14,32 @@ void close();
 SDL_Surface* loadSurface( std::string path);
 
 SDL_Texture* loadTexture( std::string path);
+
+class LTexture{
+    public:
+        // 値の初期化
+        LTexture();
+        // メモリの解放
+        ~LTexture();
+
+        // 指定したパスの画像を読み込む
+        bool loadFromFile( std::string path );
+
+        // メモリの解放
+        void free();
+
+        // 与えられた位置にテキスチャーをレンダリング
+        void render(int x, int y);
+
+        // 画像の大きさを取得
+        int getWidth();
+        int getHeight();
+
+    private:
+        // 実際のハードウェアテキスチャ
+        SDL_Texture* mTexture;
+
+        // 画像の大きさ
+        int mWidth;
+        int mHeight;
+};
--- src/main.cpp
+++ src/main.cpp
@@ -19,20 +19,14 @@ enum KeyPressSurfaces{
 ////////////////////////////////////////////////
 // レンダリング先のウインドウ
 SDL_Window* gWindow = NULL;
-// ウインドウに含まれる表面
-SDL_Surface* gScreenSurface = NULL;
-// キーに対応した画像
-SDL_Surface* gKeyPressSurfaces[ KEY_PRESS_SURFACE_TOTAL];
-// 現在表示している画像
-SDL_Surface* gCurrentSurface = NULL;
 // ウインドウレンダラー
 SDL_Renderer* gRenderer = NULL;
-// 現在表示しているテキスチャー
-SDL_Texture* gTexture = NULL;
-
+// シーンテキスチャー
+LTexture gFooTexture;
+LTexture gBackgroundTexture;
 
 ////////////////////////////////////////////////
-// 関数
+// 実装
 ////////////////////////////////////////////////
 // SDLを起動し、ウインドウを作成する
 bool init(){
@@ -70,10 +64,6 @@ bool init(){
                     printf( "SDL_image could not initialize! SDL_image Error: %s\n", IMG_GetError() );
                     success = false;
                 }
-                else{
-                    //Get window surface
-                    gScreenSurface = SDL_GetWindowSurface( gWindow );
-                }
             }
         }
     }
@@ -85,65 +75,26 @@ bool loadMedia(){
     // ロード完了フラグ
     bool success = true;
 
-    /*
-    // デフォルトサーフェイスを読み込む
-    gKeyPressSurfaces[ KEY_PRESS_SURFACE_DEFAULT ] = loadSurface( "./media/furuya_kou002_3.png" );
-    if( gKeyPressSurfaces[ KEY_PRESS_SURFACE_DEFAULT ] == NULL){
-        printf( "Failed to load default image!\n" );
-        success = false;
-    }
-
-    // upサーフェイスを読み込む
-    gKeyPressSurfaces[ KEY_PRESS_SURFACE_UP ] = loadSurface( "./media/up.png" );
-    if( gKeyPressSurfaces[ KEY_PRESS_SURFACE_UP ] == NULL){
-        printf( "Failed to load up image!\n" );
-        success = false;
-    }
-
-    // downサーフェイスを読み込む
-    gKeyPressSurfaces[ KEY_PRESS_SURFACE_DOWN ] = loadSurface( "./media/down.png" );
-    if( gKeyPressSurfaces[ KEY_PRESS_SURFACE_DOWN ] == NULL){
-        printf( "Failed to load down image!\n" );
-        success = false;
-    }
-
-    // leftサーフェイスを読み込む
-    gKeyPressSurfaces[ KEY_PRESS_SURFACE_LEFT ] = loadSurface( "./media/left.png" );
-    if( gKeyPressSurfaces[ KEY_PRESS_SURFACE_LEFT ] == NULL){
-        printf( "Failed to load left image!\n" );
-        success = false;
-    }
-
-    // rightサーフェイスを読み込む
-    gKeyPressSurfaces[ KEY_PRESS_SURFACE_RIGHT ] = loadSurface( "./media/right.png" );
-    if( gKeyPressSurfaces[ KEY_PRESS_SURFACE_RIGHT ] == NULL){
-        printf( "Failed to load right image!\n" );
-        success = false;
-    }
-    */
-    // PNGテキスチャを読み込む
-    gTexture = loadTexture( "./media/6star.png" );
-    if( gTexture == NULL ){
-        printf( "Failed to load texture image!\n" );
+    // Fooテキスチャを読み込む
+    if( !gFooTexture.loadFromFile( "./media/up.png" ) ){
+        printf( "Failed to load Foo texture image!\n" );
         success = false;
     }
     
+    // backgroundテキスチャを読み込む
+    if( !gBackgroundTexture.loadFromFile( "./media/6star.png" ) ){
+        printf( "Failed to load background texture image!\n" );
+        success = false;
+    }
+
     return success;
 }
 
 // メディアを解放し、SDLを閉じる
 void close(){
-    /*
-    // サーフェイスの解放
-    for(int i=0; i<KEY_PRESS_SURFACE_TOTAL; i++){
-        SDL_FreeSurface(gKeyPressSurfaces[i]);
-        gKeyPressSurfaces[i] = NULL;
-    }
-    gCurrentSurface = NULL;
-    */
-    SDL_DestroyTexture( gTexture );
-    gTexture = NULL;
-    
+    // 読み込んだ画像を解放する
+    gFooTexture.free();
+    gBackgroundTexture.free();    
 
     // ウインドウの破棄
     SDL_DestroyRenderer( gRenderer );
@@ -154,29 +105,6 @@ void close(){
     // SDLサブシステムを閉じる
     IMG_Quit();
     SDL_Quit();
-}
-
-// 個々の画像の読み込み
-SDL_Surface* loadSurface( std::string path){
-    // 最終適応画像
-    SDL_Surface* optimizedSurface = NULL;
-
-    // 指定したパスの画像を読み込む
-    SDL_Surface* loadedSurface = IMG_Load( path.c_str() );
-    if( loadedSurface == NULL){
-        printf( "Unable to load image %s! SDL Error: %s\n", path.c_str(), SDL_GetError() );
-    }
-    else{
-        // サーフェイスをスクリーンフォーマットにコンバート
-        optimizedSurface = SDL_ConvertSurface( loadedSurface, gScreenSurface->format, 0 );
-        if( optimizedSurface == NULL){
-            printf( "Unable to optimize image %s! SDL Error: %s\n", path.c_str(), SDL_GetError() );
-        }
-
-        // 古い読み込まれたサーフェイスを取り除く
-        SDL_FreeSurface( loadedSurface );
-    }
-    return optimizedSurface;
 }
 
 // テキスチャーの読み込み
@@ -203,6 +131,78 @@ SDL_Texture* loadTexture( std::string path ){
     return newTexture;
 }
 
+// LTextureの実装
+LTexture::LTexture(){
+    // 初期化
+    mTexture = NULL;
+    mWidth = 0;
+    mHeight = 0;
+}
+
+LTexture::~LTexture(){
+    // 解放
+    free();
+}
+
+bool LTexture::loadFromFile( std::string path ){
+    // 既存のテキスチャーを取り除く
+    free();
+
+    // 最終的なテキスチャー
+    SDL_Texture* newTexture = NULL;
+
+    // 指定したパスの画像を読み込む
+    SDL_Surface* loadedSurface = IMG_Load( path.c_str() );
+    if( loadedSurface == NULL){
+        printf( "Unable to load image %s! SDL_image Error: %s\n", path.c_str(), IMG_GetError() );
+    }
+    else{
+        // カラーキーイメージ
+        SDL_SetColorKey( loadedSurface, SDL_TRUE, SDL_MapRGB(loadedSurface->format, 0xFF, 0xFF, 0xFF ) );
+        // サーファイスピクセルからテキスチャーを作成
+        newTexture = SDL_CreateTextureFromSurface( gRenderer, loadedSurface );
+        if( newTexture == NULL ){
+            printf( "Unable to create texture from %s! SDL Error: %s\n", path.c_str(), SDL_GetError() );
+        }
+        else{
+            // 画像のサイズを取得
+            mWidth = loadedSurface->w;
+            mHeight = loadedSurface->h;
+        }
+
+        // 古い読み込まれたサーフェイスを取り除く
+        SDL_FreeSurface( loadedSurface );
+    }
+
+    // 成功を返す
+    mTexture = newTexture;
+    return mTexture != NULL;
+}
+
+void LTexture::free(){
+    // 存在しているならテキスチャーを解放する
+    if( mTexture != NULL ){
+        SDL_DestroyTexture( mTexture );
+        mTexture = NULL;
+        mWidth = 0;
+        mHeight = 0;
+    }
+}
+
+void LTexture::render( int x, int y ){
+    // レンダリング位置を設定してレンダーする
+    SDL_Rect renderQuad = { x, y, mWidth, mHeight};
+    SDL_RenderCopy( gRenderer, mTexture, NULL, &renderQuad );
+}
+
+int LTexture::getWidth(){
+    return mWidth;
+}
+
+int LTexture::getHeight(){
+    return mHeight;
+}
+
 ////////////////////////////////////////////////
 // メイン関数
 ////////////////////////////////////////////////
@@ -224,8 +224,6 @@ int main(int argc, char ** const args){
             printf( "Failed to load media!\n" );
         }
         else{
-            // 現在のサーフェイスをデフォルトに設定
-            gCurrentSurface = gKeyPressSurfaces[ KEY_PRESS_SURFACE_DEFAULT ];
 
             // アプリケーションが起動している間
             while( !quit ){
@@ -263,38 +261,11 @@ int main(int argc, char ** const args){
                 SDL_SetRenderDrawColor( gRenderer, 0xFF, 0xFF, 0xFF, 0xFF );
                 SDL_RenderClear( gRenderer );
 
-                // 左上コーナーのビューポート
-                SDL_Rect topLeftViewport;
-                topLeftViewport.x = 0;
-                topLeftViewport.y = 0;
-                topLeftViewport.w = SCREEN_WIDTH/2;
-                topLeftViewport.h = SCREEN_HEIGHT/2;
-                SDL_RenderSetViewport( gRenderer, &topLeftViewport );
+                // Backgroundテキスチャーをスクリーンにレンダーする
+                gBackgroundTexture.render( 0, 0 );
 
-                // スクリーンにテキスチャーをレンダーする
-                SDL_RenderCopy( gRenderer, gTexture, NULL, NULL);
-
-                // 右上コーナーのビューポート
-                SDL_Rect topRightViewport;
-                topRightViewport.x = SCREEN_WIDTH/2;
-                topRightViewport.y = 0;
-                topRightViewport.w = SCREEN_WIDTH/2;
-                topRightViewport.h = SCREEN_HEIGHT/2;
-                SDL_RenderSetViewport( gRenderer, &topRightViewport );
-
-                // スクリーンにテキスチャーをレンダーする
-                SDL_RenderCopy( gRenderer, gTexture, NULL, NULL);
-
-                // 下コーナーのビューポート
-                SDL_Rect bottomViewport;
-                bottomViewport.x = 0;
-                bottomViewport.y = SCREEN_HEIGHT/2;
-                bottomViewport.w = SCREEN_WIDTH;
-                bottomViewport.h = SCREEN_HEIGHT/2;
-                SDL_RenderSetViewport( gRenderer, &bottomViewport );
-
-                // スクリーンにテキスチャーをレンダーする
-                SDL_RenderCopy( gRenderer, gTexture, NULL, NULL);
+                // Fooテキスチャーをスクリーンにレンダーする
+                gFooTexture.render( 0, 50 );
 
                 // スクリーンの更新
                 SDL_RenderPresent( gRenderer );
